0e4bfe0da0e2c9f672ddf2733ee92f3d282a1a52
zacharytq <zacharytq@gmail.com>
2021-01-06 10:11:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+function map(array, func){
+    return array.map(x => func(x))
+    
+}
+
+function reduce(array, func, start) {
+    const reducer = (accumulator, item) => {
+        return func(accumulator, item)
+      };
+      if (!!start) {
+        return array.reduce(reducer, start)
+      } else {
+        return array.reduce(reducer)
+      }
+    
+}
